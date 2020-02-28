0d6e35a7533a51a0238015fff39c7b554d45f72a
Clark Fennell <clark.fennell@flauntdigital.com>
2020-02-28 16:00:53 +0000

    initial pages added to php

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+</body>
+<footer>
+</footer>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/dist/script<?php
+    if(!defined('BROWSERSYNC'))
+      echo '.'.filemtime(get_template_directory().'/assets/js/dist/script.min.js');
+    ?>.min.js" async defer></script>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<head>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style<?php
+    if(!defined('BROWSERSYNC'))
+      echo '.'.filemtime(get_template_directory().'/assets/css/style.min.css');
+    ?>.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#000">
+    <meta http-equiv="X-UA-Compatible" content="IE=11">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5QPTJZH');</script>
+    <!-- End Google Tag Manager -->
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QPTJZH"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <script>
+        var ajax_url = "<?php echo admin_url( 'admin-ajax.php' ); ?>";
+    </script>
+    <header>
+    </header>
+    <main>
