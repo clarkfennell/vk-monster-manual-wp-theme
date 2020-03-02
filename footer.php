e479d8d34c028d514e331e957035f1aea8b6ed7d
Clark Fennell <clark.fennell@flauntdigital.com>
2020-03-02 11:03:51 +0000

    adjusting the curivse handwriting font

--- footer.php
+++ footer.php
@@ -2,9 +2,5 @@
 </body>
 <footer>
 </footer>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/dist/script<?php
-    if(!defined('BROWSERSYNC'))
-      echo '.'.filemtime(get_template_directory().'/assets/js/dist/script.min.js');
-    ?>.min.js" async defer></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,27 +1,31 @@
 <!DOCTYPE HTML>
 <head>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.<?php echo filemtime(get_template_directory().'/assets/css/style.min.css'); ?>.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#000">
     <meta http-equiv="X-UA-Compatible" content="IE=11">
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5QPTJZH');</script>
-    <!-- End Google Tag Manager -->
 </head>
-<body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QPTJZH"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
-    <script>
-        var ajax_url = "<?php echo admin_url( 'admin-ajax.php' ); ?>";
-    </script>
-    <header>
+<body class="frame">
+    <header class='row col-sm-12'>
+        <div class='col-sm-<?php echo (has_nav_menu('primary')) ? 3 : 3 ?> page-header-logo'>
+            <a aria-label="home button" alt="Val Killmeh's Monster Manual" title="Val Killmeh's Monster Manual" href='<?php echo home_url('/'); ?>' class='logo-link' rel="spa">
+                <img src="https://via.placeholder.com/200x200" />
+            </a>
+        </div>
+        <div class="col-sm-<?php echo (has_nav_menu('primary')) ? 3 : 9 ?>">
+            <h1>
+                <small>Val Killmeh's</small>
+                <br/>
+                Monster Manual
+            </h1>
+        </div>
+        <?php if (has_nav_menu('primary')) { ?>
+            <nav class='col-sm-6 page-header-nav'>
+                <button aria-label="toggle menu" alt="toggle menu" class='page-header-nav-button mobile-h5' data-attr-alt-text='<?php _e('Close'); ?>'><?php _e('Menu'); ?></button>
+                <?php wp_nav_menu(array(
+                    'theme_location' => 'primary',
+                )); ?>
+            </nav>
+        <?php } ?>
     </header>
-    <main>
+    <main class>
